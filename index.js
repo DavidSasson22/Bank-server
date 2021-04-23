db7dafa0c5516908059755636cbb3f706a4d41ca
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-04-23 18:23:04 +0300

    cors added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('./dataBase/mongooseConfig');
 const express = require('express');
+const cors = require('cors')
 const usersRouter = require('./routs/usersRouts');
 const userRouter = require('./routs/userRouts');
 
@@ -7,6 +8,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/users', usersRouter);
 app.use('/api/users', userRouter);
 
